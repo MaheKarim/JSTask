794ff1538c835c4ef073751bb408778e57c3f73b
MaheKarim <mahekarim@gmail.com>
2019-05-17 16:59:05 +0600

    Arithmatic Operation

--- hello_world.js
+++ hello_world.js
@@ -2,5 +2,13 @@
 var eggPrice = 5;
 var totalEgg = 12;
 var totalCostOfEgg = eggPrice * totalEgg;
+var totalCostOfEggPlus = eggPrice + totalEgg;
+var totalCostOfEggMinus = eggPrice - totalEgg;
+var totalCostOfEggDivided = eggPrice / totalEgg;
+
+
 
 console.log("Total price:",totalCostOfEgg,"BDT");
+console.log("Total price:",totalCostOfEggPlus,"BDT");
+console.log("Total price:",totalCostOfEggMinus,"BDT");
+console.log("Total price:",totalCostOfEggDivided,"BDT");
