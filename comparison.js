d8ef440beb9432b3d3a140b8059b478a386e3492
MaheKarim <mahekarim@gmail.com>
2019-05-17 18:06:41 +0600

    Nested If Else Condition

---
+++ comparison.js
@@ -0,0 +1,16 @@
+let personOne = "Jamal";
+let personTwo = "Kamal";
+let areTheyBrother = true;
+
+if ("Jamal" == personOne) {
+    if ("Kamal" == personTwo) {
+        if (areTheyBrother) {
+            console.log("Jamal and Kamal Are Brothers!");
+        } else {
+            console.log("They are not brother");
+        }
+    }
+}
+
+
+
