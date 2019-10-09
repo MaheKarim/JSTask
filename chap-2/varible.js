bd14d0d1c9bebd77fbabb6f33a4b9c51a0ab614c
Mahekarim <mahekarim@gmail.com>
2019-10-09 19:48:21 +0600

    Variable Declare

---
+++ chap-2/varible.js
@@ -0,0 +1,11 @@
+var name = 'Mahe Karim'
+var age = 25
+
+console.log('I am Mahe Karim direct text')
+console.log('I am ' + name)
+console.log('My age is ' + age)
+
+ // string + number = string
+
+ console.log('9' + 9) // string + number = string
+ console.log(9 + 9) // number + number 
