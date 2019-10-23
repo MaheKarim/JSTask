a6070729c356ee0b8979082061f333c6dcd1a045
Mahekarim <mahekarim@gmail.com>
2019-10-23 19:06:46 +0600

    :ZZZ:

--- chap-2/hello-world.js
+++ chap-2/hello-world.js
@@ -1,1 +1,14 @@
-console.log('mahe')
+// var age = 24;
+// var name = 'Mahe Karim';
+// console.log('Hello World!');
+// console.log('Hello! My name is ' + name);
+// console.log('My age is ' + age);
+
+//console.log(age);
+
+var change = '9';
+// console.log('9' + change);
+// console.log(change + change);
+console.log( 9 + change );
+console.log('9'+change);
+console.log(9+9);
